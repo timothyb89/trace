7fc6eb361c79b1f7a68c8dddb4e92d22269f8489
Tim <timothyb89@gmail.com>
2015-09-22 13:40:49 -0600

    Add timing utility functions

--- src/main/java/org/timothyb89/trace/util/F.java
+++ src/main/java/org/timothyb89/trace/util/F.java
@@ -1,6 +1,9 @@
 package org.timothyb89.trace.util;
 
 import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.DoubleConsumer;
+import java.util.function.Supplier;
 
 /**
  * Various functional utilities.
@@ -22,6 +25,52 @@ public class F {
 		for (T object : collection) {
 			consumer.accept(object, index++, collection.size());
 		}
+	}
+
+	public static TimedCompletableFuture<Void> timeVoid(Runnable func) {
+		long start = System.nanoTime();
+		func.run();
+		long diff = System.nanoTime() - start;
+
+		TimedCompletableFuture<Void> ret = new TimedCompletableFuture<>();
+		ret.completeTime((double) diff / 1000000000.0);
+		return ret;
+	}
+
+	public static <T> TimedCompletableFuture<T> time(Supplier<T> func) {
+		long start = System.nanoTime();
+		T result = func.get();
+		long diff = System.nanoTime() - start;
+
+		TimedCompletableFuture<T> ret = new TimedCompletableFuture<>();
+		ret.completeTime((double) diff / 1000000000.0);
+		ret.complete(result);
+		return ret;
+	}
+
+	public static class TimedCompletableFuture<T> extends CompletableFuture<T> {
+
+		private DoubleConsumer timeConsumer;
+		private Double time;
+
+		public TimedCompletableFuture<T> thenAcceptTime(DoubleConsumer timeConsumer) {
+			this.timeConsumer = timeConsumer;
+
+			if (time != null) {
+				timeConsumer.accept(time);
+			}
+
+			return this;
+		}
+
+		public void completeTime(Double time) {
+			this.time = time;
+
+			if (timeConsumer != null) {
+				timeConsumer.accept(time);
+			}
+		}
+
 	}
 
 }
